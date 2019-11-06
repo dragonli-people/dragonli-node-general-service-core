43a1a80e31ca9071d38c7e6469118e0a954f4e0f
315920958@qq.com <315920958@qq.com>
2019-11-06 18:06:03 +0800

    mysql handler

--- appinithandlers/AppInitMysqlHandler.js
+++ appinithandlers/AppInitMysqlHandler.js
@@ -1,27 +1,38 @@
 const {MysqlHandlerFactory} = require('dragonli-node-tools');
 
 module.exports = class  {
-    constructor(varName,hostConfigKey,portConfigKey,userConfKey,passwdConfKey,dbConfigKey,keyIsValue=false){
+    constructor(varName,...paras){
+        var hostConfigKey,portConfigKey,dbConfigKey,userConfKey,passwdConfKey,keyIsValue=false,jdbcUrl=null;
+        if(paras.length === 5 || paras.length === 6)
+            [hostConfigKey,portConfigKey,dbConfigKey,userConfKey,passwdConfKey,keyIsValue] = paras;
+        if(paras.length === 3 || paras.length === 4)
+            [jdbcUrl,userConfKey,passwdConfKey,keyIsValue] = paras;
+
+        keyIsValue=keyIsValue||false
         this.HANDLER_KEY = varName;
         this.varName = varName;
+        this.jdbcUrl = jdbcUrl;
         this.hostConfigKey = hostConfigKey;
         this.portConfigKey = portConfigKey;
         this.userConfKey = userConfKey;
         this.passwdConfKey = passwdConfKey;
         this.dbConfigKey = dbConfigKey;
-        this.keyIsValue = false;
+        this.keyIsValue = keyIsValue;
     }
 
     async init(app,DATA_POOL,CONFIG_POOL){
-        var host = this.hostConfigKey,port = this.portConfigKey
+        var jdbcUrl = this.jdbcUrl ,host = this.hostConfigKey,port = this.portConfigKey
             ,user = this.userConfKey,passwd = this.passwdConfKey,db = this.dbConfigKey;
         if( !this.keyIsValue ){
+            jdbcUrl = jdbcUrl && CONFIG_POOL[jdbcUrl] ;
             host = host && CONFIG_POOL[host] ;
             port = port && CONFIG_POOL[port] ;
             user = user && CONFIG_POOL[user] ;
             passwd = passwd && CONFIG_POOL[passwd] ;
             db = db && CONFIG_POOL[db] ;
         }
+        if(jdbcUrl)
+            [host,port,db] = jdbcUrl.replace(/^.*mysql\:\/\//gi,'').replace(/\?.*$/gi,'').replace(':','\/').split('\/');
         port = port && parseInt(port) || null;
         if(!host || !port || !user || !passwd || !db)
             throw new Error(`one of parameter is empty ! host:${host} , port:${port} , user:${user} , passwd:${passwd} , db:${db} `);
