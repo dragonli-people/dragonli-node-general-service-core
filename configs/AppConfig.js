25fe8d3b77611335628ec7f0a7984fb8e1e38cab
315920958@qq.com <315920958@qq.com>
2019-11-06 10:53:26 +0800

    assert ioc

--- configs/AppConfig.js
+++ configs/AppConfig.js
@@ -1,4 +1,4 @@
-const {AppConfig} = require('dragonli-node-service-core');
+const {AppConfig,AssertCheck} = require('dragonli-node-service-core');
 const InitTelnetCommand = require('../appinithandlers/InitTelnetCommand');
 const GeneralCommand = require('../commands/GeneralCommand');
 const HttpPortConfig = require('./HttpPortConfig');
@@ -9,8 +9,13 @@ module.exports = class extends AppConfig {
     constructor(){
         super();
         this.addAppInitConfigHandlers([HttpPortConfig,TelnetCommandConfig]);
-        this.addAppInitHandlers([InitTelnetCommand]);
+        this.addAppInitHandlers([
+            InitTelnetCommand,
+            {HANDLER_KEY:'assertCheck',init:app=>app.putToPool('assert',AssertCheck)},
+        ]);
         this.registerCommand(GeneralCommand);
         this.addControllerFilterHandlers([CorsFilter]);
+        this.addControllerIocKeys(['assert']);//auto ioc those variables
+
     }
 }
