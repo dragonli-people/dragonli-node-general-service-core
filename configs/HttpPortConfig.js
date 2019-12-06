817aa2fbb32c930a8cd651bd330acd533cb42434
315920958@qq.com <315920958@qq.com>
2019-12-06 14:06:46 +0800

    bug fix

--- configs/HttpPortConfig.js
+++ configs/HttpPortConfig.js
@@ -1,12 +1,12 @@
 class HttpPortConfig {
     constructor(portConfigPath,varName){
-        HttpPortConfig.portConfigPath = portConfigPath;
+        portConfigPath && ( HttpPortConfig.portConfigPath = portConfigPath );
         this.HANDLER_KEY = varName || 'HttpPortConfig';
     }
 
     async init(app,DATA_POOL,CONFIG_POOL){
         var port = process.env.HTTP_PORT || app.config.port;
-        HttpPortConfig.portConfigPath && ( port = CONFIG_POOL[HttpPortConfig.portConfigPath] || path );
+        HttpPortConfig.portConfigPath && ( port = CONFIG_POOL[HttpPortConfig.portConfigPath] || port );
         port && (app.putConfigToPool('HTTP_PORT',parseInt(port)));
         port && (app.config.setPort(port));
     }
